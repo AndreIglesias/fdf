61e9bf016382a0054d5014c836d25df080225f99
kateincoding <kateincoding@gmail.com>
2021-08-25 02:36:17 +0200

    alg test

--- src/main.c
+++ src/main.c
@@ -29,13 +29,16 @@ int		exit_win(void *p __attribute__((unused)))
 
 int		key_win(int key, void *p __attribute__((unused)))
 {
+	t_fdf	*fdf;
+
+	fdf = ft_fdf(NULL);
 	if (key == 0xFF1B)
 	{
 		exit(0);
 	}
 	if (key == 0xFF20)
 	{
-		if (fdf->view = 3)
+		if (fdf->view == 3)
 			fdf->view = 2;
 		else
 			fdf->view = 3;
@@ -43,6 +46,7 @@ int		key_win(int key, void *p __attribute__((unused)))
 	return (0);
 }
 
+/*
 static void	test()
 {
 	int		a;
@@ -65,13 +69,39 @@ static void	test()
 	mlx_string_put(fdf->mlx, fdf->win, fdf->res[0] / 2 - 35, fdf->res[1] / 2, 0xFFFFFF, "FDF...");
 	mlx_loop(fdf->mlx);
 }
+*/
+
+static void	kate_test()
+{
+	int		a;
+	t_fdf	*fdf = ft_fdf(NULL);
+
+	a = 0x11223344;
+	fdf->local_endian = 0;
+	if (((unsigned char *)&a)[0] == 0x11)
+		fdf->local_endian = 1;
+	if (!(fdf->mlx = mlx_init()))
+		return ;
+	mlx_get_screen_size(fdf->mlx, &fdf->res[0], &fdf->res[1]);
+	fdf->res[0] -= 10;
+	fdf->res[1] -= 10;
+	if (!(fdf->win = mlx_new_window(fdf->mlx, fdf->res[0], fdf->res[1], "wireframe")))
+		return ;
+	mlx_hook(fdf->win, CROSS_EVENT, CROSS_MASK, exit_win, 0);
+	mlx_key_hook(fdf->win, key_win, fdf->mlx);
+	mlx_loop_hook(fdf->mlx, render_lines, fdf);
+	// ft_plot(fdf->mlx, fdf->win, fdf->res, fdf->local_endian);
+	// mlx_string_put(fdf->mlx, fdf->win, fdf->res[0] / 2 - 35, fdf->res[1] / 2, 0xFFFFFF, "FDF...");
+	mlx_loop(fdf->mlx);
+}
 
 static void	fdf(char *file)
 {
 	t_fdf	*fdf;
 
 	fdf = ft_fdf(file);
-	test();
+	// test();
+	kate_test();
 	return ;
 }
 
