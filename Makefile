131e3cca0b5c2f33371ac0d8c3b3d6a106611654
kateincoding <kateincoding@gmail.com>
2021-09-14 02:55:15 -0500

    [segmentation fault fixed]

--- Makefile
+++ Makefile
@@ -61,7 +61,7 @@ CFLAGS		=
 CFLAGS		=	-Wall -Wextra -ansi -pedantic -g
 	endif
 else
-CFLAGS		=	-Wall -Wextra
+CFLAGS		=	-Wall -Wextra -g
 endif
 
 ifndef VERBOSE
--- src/main.c
+++ src/main.c
@@ -19,10 +19,6 @@ t_fdf	*ft_fdf(char *name)
 
 	if (name)
 		load_map(&fdf, name);
-	fdf.init->x = 10;
-	fdf.init->y = 10;
-	fdf.end->x = 50;
-	fdf.end->y = 30;
 	return (&fdf);
 }
 
@@ -87,7 +83,7 @@ static void	test()
 }
 */
 
-/*
+
 static void	init_map(void)
 {
 	t_fdf	*fdf;
@@ -96,10 +92,16 @@ static void	init_map(void)
 	fdf->coord_x = 0;
 	fdf->coord_y = 0;
 	fdf->side_z = 1;
-	fdf->side_x = cos(M_PI / 3) * fdf->side_z;
-	fdf->side_y = fdf->side_x * sin(M_PI / 6);
+	fdf->init = malloc(sizeof(t_pixel));
+	fdf->end = malloc(sizeof(t_pixel));
+	fdf->init->x = 10;
+	fdf->init->y = 10;
+	fdf->end->x = 300;
+	fdf->end->y = 300;
+//	fdf->side_x = cos(M_PI / 3) * fdf->side_z;
+//	fdf->side_y = fdf->side_x * sin(M_PI / 6);
 }
-*/
+
 /*
 static void	kate_test(void)
 {
@@ -163,6 +165,7 @@ static void	simple_test(void)
 	t_fdf	*fdf;
 
 	fdf = ft_fdf(NULL);
+	init_map();
 	fdf->mlx = mlx_init();
 	fdf->win = mlx_new_window(fdf->mlx, 700, 700, "wireframe");
 	bresenham_line();
@@ -174,7 +177,7 @@ static void	fdf(char *file)
 {
 	t_fdf	*fdf;
 
-	fdf = ft_fdf(file);
+	// fdf = ft_fdf(file);
 	// test();
 	// kate_test();
 	simple_test();
