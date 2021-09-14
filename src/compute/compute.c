65b2dec22c17012589d91bf11ff023597eb13ff7
kateincoding <kateincoding@gmail.com>
2021-09-14 06:57:26 -0500

    [norm]

--- src/compute/compute.c
+++ src/compute/compute.c
@@ -12,37 +12,37 @@
 
 #include "fdf.h"
 
-int     max_calculator(int a, int b)
-{
-    if (a > b)
-        return (a);
-    return (b);
-}
+/*
+** max_calculator: calculate the max number between 2
+*/
 
-int     module(int a)
+int	max_calculator(int a, int b)
 {
-    if (a < 0)
-        a = a * -1;
-    return (a);
-}
-
-void    convert_isometric(t_pixel *px, int z, t_fdf *fdf)
-{
-    if (fdf->view == 3)
-    {
-        px->x = (px->x - px->y)*cos(0.7);
-        px->y = (px->x + px->y)*sin(0.7) - z;
-    }
+	if (a > b)
+		return (a);
+	return (b);
 }
 
 /*
-void    compute_horizontal(t_fdf *fdf, int x, int y)
-{
-    int delta_x;
-    int delta_y;
-
-    delta_x = x - fdf->mapx / 2;
-    delta_y = x - fdf->mapy / 2;
-    fdf->y0 = fdf->side_y;
-}
+** module: apply module to a number
 */
+
+int	module(int a)
+{
+	if (a < 0)
+		a = a * -1;
+	return (a);
+}
+
+/*
+** convert_isometric: reasign the values of x y to a isometric
+*/
+
+void	convert_isometric(t_pixel *px, int z, t_fdf *fdf)
+{
+	if (fdf->view == 3)
+	{
+		px->x = (px->x - px->y) * cos(0.7);
+		px->y = (px->x + px->y) * sin(0.7) - z;
+	}
+}
