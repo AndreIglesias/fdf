e226939a758f0266f9497340d03230709fcf4379
kateincoding <kateincoding@gmail.com>
2021-10-05 15:48:03 -0500

    [norm]

--- src/compute/compute.c
+++ src/compute/compute.c
@@ -6,7 +6,7 @@
 /*   By: ksoto <ksoto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/11 20:40:11 by ciglesia          #+#    #+#             */
-/*   Updated: 2021/09/27 08:12:06 by ksoto            ###   ########.fr       */
+/*   Updated: 2021/10/05 15:47:45 by ksoto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,21 +56,8 @@ void	set_color(t_fdf *fdf)
 	long	z;
 	long	z1;
 
-	ft_putnbr(fdf->mapy);
-	write(1, ",", 2);
-	ft_putnbr(fdf->mapx);
-
-	write(1, "B", 1);
 	z = fdf->map[(int)fdf->init->y][(int)fdf->init->x].z;
-
-	write(1, "U", 1);
-
-	ft_putnbr(fdf->end->y);
-	write(1, ", ", 2);
-	ft_putnbr(fdf->end->x);
-
 	z1 = fdf->map[(int)fdf->end->y][(int)fdf->end->x].z;
-	write(1, "G\n", 2);
 	if (z || z1)
 		fdf->color = 0xe80c0c;
 	else
